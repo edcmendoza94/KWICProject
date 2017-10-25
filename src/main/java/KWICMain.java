07026d66d437c0c1642659b30b5309a5ffc63f94
edcmendoza94 <edcmendoza94@gmail.com>
2017-10-24 19:24:20 -0700

    Uploading to GitHub for IT 372 Assignment 07

---
+++ src/main/java/KWICMain.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+public class KWICMain {
+    public void readFile() {
+
+    }
+
+    public void sendToEnd() {
+
+    }
+
+    public void sortAlphabetical() {
+
+    }
+
+    public String[] printSorts() {
+        return null;
+    }
+
+    public File outputFileOfSorts() {
+        return null;
+    }
+}
